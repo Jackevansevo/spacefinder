58576121da408ab5cabc34a5105ec627e28bc28d
Jack Evans <jack@evans.gb.net>
2016-03-23 10:44:34 +0000

    refactor studyspace tags

--- spacefinder/templatetags/studyspace_extra.py
+++ spacefinder/templatetags/studyspace_extra.py
@@ -7,8 +7,7 @@ busyness_scores = {
 }
 
 
+@register.filter(name='get_busyness_score')
 def get_busyness_score(value):
     """Converts float 'busyness' rating to human readable value."""
     return busyness_scores.get(round(float(value)))
-
-register.filter('get_busyness_score', get_busyness_score)
