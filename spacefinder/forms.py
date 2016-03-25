7fd78bbab970082ac54a838fb9bb5a885dc46156
Jack Evans <jack@evans.gb.net>
2016-03-25 18:01:11 +0000

    Unfucked user auth

--- spacefinder/forms.py
+++ spacefinder/forms.py
@@ -1,19 +1,21 @@
 from django import forms
-from .models import Department
+from .models import Student
 from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'email')
+        fields = ('username', 'email', 'password')
+        widgets = {'password': forms.PasswordInput()}
 
     # Add an extra password confirmation field
-    password = forms.CharField(widget=forms.PasswordInput())
     confirm_password = forms.CharField(widget=forms.PasswordInput())
 
     def clean_password(self):
         """Checks if the two passwords entered by the user are the same"""
+        print(self.data['password'])
+        print(self.data['confirm_password'])
         if self.data['password'] != self.data['confirm_password']:
             raise forms.ValidationError('Passwords are not the same')
         return self.data['password']
@@ -23,11 +25,10 @@ class UserForm(forms.ModelForm):
         return super(UserForm, self).clean(*args, **kwargs)
 
 
-class StudentForm(forms.Form):
-    department = forms.ModelChoiceField(
-        empty_label="Select Department",
-        queryset=Department.objects.all(), widget=forms.Select())
-    avatar = forms.ImageField(required=False)
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ('avatar', 'department')
 
 
 class LoginForm(forms.ModelForm):
