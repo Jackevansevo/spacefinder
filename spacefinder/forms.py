778540515d7e6feec7a6ddbcd8028265b6b99e95
Jack Evans <jack@evans.gb.net>
2016-03-25 12:38:16 +0000

    Got rid of META classe, as they didn't actually serve any purpose

--- spacefinder/forms.py
+++ spacefinder/forms.py
@@ -1,14 +1,26 @@
 from .models import Student
 from django.contrib.auth.models import User
 from django import forms
+from .models import Department
 
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-    confirm_password = forms.CharField(
-        widget=forms.PasswordInput(),
-        label="Confirm Password"
-    )
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={"placeholder": "Username", "class": "form-control"}
+    ))
+
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={"placeholder": "Email", "class": "form-control"}
+    ))
+
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={"placeholder": "Password", "class": "form-control"}
+    ))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(
+        attrs={"placeholder": "Confirm Password", "class": "form-control"}
+    ))
 
     def clean_password(self):
         """Checks if the two passwords entered by the user are the same"""
@@ -20,31 +32,22 @@ class UserForm(forms.ModelForm):
         self.clean_password()
         return super(UserForm, self).clean(*args, **kwargs)
 
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
 
 class StudentForm(forms.ModelForm):
-    class Meta:
-        model = Student
-        fields = ('department', 'avatar')
+    department = forms.ModelChoiceField(
+        empty_label="Select Department",
+        queryset=Department.objects.all(), widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            }
+        ))
+    avatar = forms.ImageField(required=False)
 
 
 class LoginForm(forms.ModelForm):
     username = forms.CharField(widget=forms.TextInput(
-        attrs={
-            "placeholder": "Username",
-            "class": "form-control"
-        }
+        attrs={"placeholder": "Username", "class": "form-control"}
     ))
     password = forms.CharField(widget=forms.PasswordInput(
-        attrs={
-            "placeholder": "Password",
-            "class": "form-control"
-        }
+        attrs={"placeholder": "Password", "class": "form-control"}
     ))
-
-    class Meta:
-        model = User
-        fields = ('username', 'password')
