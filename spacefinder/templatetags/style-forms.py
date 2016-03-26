24ed8a1d3b9d9310b044bcf9c78ff5e38fead3aa
Jack Evans <jack@evans.gb.net>
2016-03-26 15:31:52 +0000

    Moved css styles out of forms and into templatetags

---
+++ spacefinder/templatetags/style-forms.py
@@ -0,0 +1,15 @@
+from django.template import Library
+
+register = Library()
+
+
+@register.filter(name='placeholder')
+def placeholder(value, token):
+    value.field.widget.attrs["placeholder"] = token
+    return value
+
+
+@register.filter(name="addClass")
+def addClass(value, newClass):
+    value.field.widget.attrs["class"] = newClass
+    return value
