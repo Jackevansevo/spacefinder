46014ca968f6fb4cdb62684163a87d8e7b52c5e9
Jack Evans <jack@evans.gb.net>
2016-03-27 12:15:37 +0100

    Added missing semicolons

--- spacefinder/static/spacefinder/popup-dialogs.js
+++ spacefinder/static/spacefinder/popup-dialogs.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
     $('.modal').each(function(index) {
         $(this).on('show.bs.modal', function (e) {
             $('.modal-dialog').velocity('transition.expandIn');
-        })
-    })
+        });
+    });
 
     // Keep popup dialogs on mouse house and close when the mouse leaves
     $('.popoverButton').on("mouseenter", function (e) {
@@ -17,7 +17,7 @@ $(document).ready(function() {
         var _this = this;
         setTimeout(function () {
             if (!$(".popover:hover").length) {
-                $(_this).popover("hide")
+                $(_this).popover("hide");
             }
         }, 100);
     });
@@ -38,16 +38,16 @@ $(document).ready(function() {
             placement: 'bottom',
             content: _content
         };
-    }
+    };
 
     // Show the login popup dialog
     var loginDialog = popoverDialog($("#loginPopup").html());
     $('#loginButton').popover(loginDialog).on("mouseenter", function () {
         $('input:text:visible:first').focus();
-    })
+    });
 
     // Show the user profile popup dialog
     var profileDialog = popoverDialog($("#profilePopup").html());
     $('#profileButton').popover(profileDialog);
 
-})
+});
