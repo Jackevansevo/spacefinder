087dd2b5f5589aa48ace37b925bd468c81530d45
Jack Evans <jack@evans.gb.net>
2016-03-27 17:34:54 +0100

    Added code to stop previous animations once new ones have been triggered

--- spacefinder/static/spacefinder/emoji-slider.js
+++ spacefinder/static/spacefinder/emoji-slider.js
@@ -9,23 +9,32 @@ $(document).ready(function() {
     $((emojis[$("#slider").val()-1])).css("opacity", "1");
 
     // Get slider position
-    sliderPos = $("#slider").val();
+    var sliderPos = $("#slider").val();
 
     // Update the slider whenever the emojis are changed
     $("#slider").on("input change", function() {
-        newPos = $("#slider").val();
+        var newPos = $("#slider").val();
 
         // Only animate emojis if slider position is different
         if(sliderPos != newPos) {
+
+            var index = newPos-1;
+
+            // Stop any other still running animations
+            for(var i=0; i < emojis.length; i ++) {
+                if(i != index) {
+                    $(emojis[index]).velocity("stop");
+                }
+            }
 
             // Shade all the emojis
             $(".emoji").css("opacity", "0.2");
 
             // Animate the selected emoji
-            $(emojis[$(this).val()-1]).velocity('callout.bounce');
+            $(emojis[index]).velocity('callout.bounce');
 
             // Unshade the selected one
-            $((emojis[$(this).val()-1])).css("opacity", "1");
+            $((emojis[index])).css("opacity", "1");
         }
         sliderPos = $("#slider").val();
     });
