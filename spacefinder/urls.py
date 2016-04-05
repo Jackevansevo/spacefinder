8c0b2ecc08a8c7cba529fc02b22d0b41e75bc626
Jack Evans <jack@evans.gb.net>
2016-04-05 16:46:05 +0100

    Removed login url as it was no longer necessary

--- spacefinder/urls.py
+++ spacefinder/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='user_login'),
     url(r'^logout/$', views.user_logout, name='user_logout'),
     url(r'^space/(?P<slug>[\w-]+)/$', views.studyspace, name='studyspace'),
     url(r'^profile/(?P<slug>[\w-]+)/$', views.profile, name='profile'),
