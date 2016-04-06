b3076ad2da6fb79fe06efbfc937d942e0c50492c
Jack Evans <jack@evans.gb.net>
2016-04-06 15:38:51 +0100

    Admin panel now shows more useful information

--- spacefinder/admin.py
+++ spacefinder/admin.py
@@ -4,15 +4,18 @@ from .models import Department, StudySpace, Rating, Student
 
 
 class StudySpaceAdmin(admin.ModelAdmin):
+    list_display = ('space_name', 'department', 'avg_rating')
     prepopulated_fields = {'slug': ('space_name',)}
     readonly_fields = ("avg_rating",)
 
 
 class StudentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'department', 'karma')
     prepopulated_fields = {'slug': ('user',)}
 
 
 class RatingAdmin(admin.ModelAdmin):
+    list_display = ('rating', 'studyspace', 'student', 'timestamp')
     readonly_fields = ("timestamp",)
 
 admin.site.register(Department)
