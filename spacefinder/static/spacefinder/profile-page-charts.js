5e347403582de7cdf381aaa81a6a921b00f2314c
Jack Evans <jack@evans.gb.net>
2016-04-06 02:19:46 +0100

    Added charts to the profile page

---
+++ spacefinder/static/spacefinder/profile-page-charts.js
@@ -0,0 +1,36 @@
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var options = {
+        curveType: 'function',
+        pointSize: 7,
+        backgroundColor: 'transparent',
+        legend: 'none',
+        hAxis: {
+            maxAlternation: 1,
+            maxTextLines: 1,
+            slantedText: 'false',
+        },
+        vAxis: {
+            gridlines: {
+                color: 'transparent'
+            },
+            ticks: [
+                {v: 0, f: ''},
+                {v: 1, f: 'Busy'},
+                {v: 2, f: 'Quite-Busy'},
+                {v: 3, f: 'Average'},
+                {v: 4, f: 'Quite-Empty'},
+                {v: 5, f: 'Empty'}
+            ],
+            format: '#',
+        }
+    };
+
+    latest_ratings.unshift(["Time", "Rating"]);
+    console.log(latest_ratings);
+    var latest_ratings_chart = new google.visualization.LineChart(document.getElementById('latest-ratings-chart'));
+    latest_ratings_chart.draw(google.visualization.arrayToDataTable(latest_ratings), options);
+}
