177ed9b1052d88b67b17eb77342d951b0d5ff90e
Jack Evans <jack@evans.gb.net>
2016-04-09 16:31:00 +0100

    Replaced popup dialogs with modal forms, since popover dialogs don't play nice on mobile devices

--- spacefinder/static/spacefinder/popup-dialogs.js
+++ spacefinder/static/spacefinder/popup-dialogs.js
@@ -1,42 +1,6 @@
 $(document).ready(function() {
 
-    // Popover dialog constructor
-    var popoverDialog = function(content) {
-        return {
-            trigger: 'manual',
-            placement: 'bottom',
-            html: true,
-            content: content,
-        };
-    };
-
-    // Show the login popup dialog
-    var loginDialog = popoverDialog($("#loginPopup").html());
-    $('#loginButton').popover(loginDialog);
-
-    // Show the user profile popup dialog
-    var profileDialog = popoverDialog($("#profilePopup").html());
-    $('#profileButton').popover(profileDialog);
-
-    // Show popover on button clicks
-    $('.popoverButton').click(function(e) {
-        $(this).popover('toggle');
-        e.stopPropagation();
-    });
-
-    // Hide popovers when any other html element is clicked
-    $('html').click(function(e) {
-        if(!$(e.target).parents('.popover-content').length && !$(e.target).hasClass('popover-content')) {
-            $('.popoverButton').popover('hide');
-        }
-    });
-
-    // Show the login popover if errors are present
-    if($('#loginErrorMessage').length) {
-        $('#loginButton').popover('show');
-    }
-
-    // Shows the registration dialogif there are errors present
+    // Shows the registration dialog if there are errors present
     if($('#registration-error-msg').length) {
         $('#registerModal').show('show');
         setTimeout(function (){
@@ -44,9 +8,18 @@ $(document).ready(function() {
         }, 200);
     }
 
+    // Shows the registration dialog if there are errors present
+    if($('#loginErrorMessage').length) {
+        $('#loginModal').show('show');
+        setTimeout(function (){
+            $('#LoginUsername').focus();
+        }, 200);
+    }
+
     // Hide the modal on click
     $('button.close').click(function() {
         $('#registerModal').hide();
+        $('#loginModal').hide();
     });
 
     // Make the registration form appear
@@ -56,6 +29,14 @@ $(document).ready(function() {
         $('#LoginSelectDepartmentDropDown').css("color", "#999");
         setTimeout(function (){
             $('#RegisterUsername').focus();
+        }, 200);
+    });
+
+    // Make the login form appear
+    $("#loginModal").on('show.bs.modal', function () {
+        $('.modal-dialog').velocity('transition.bounceDownIn');
+        setTimeout(function (){
+            $('#LoginUsername').focus();
         }, 200);
     });
 
