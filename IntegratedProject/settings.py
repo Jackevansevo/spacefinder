8451821bc54b137b3de39ba06663a9b84939e9ee
Jack Evans <jack@evans.gb.net>
2016-04-10 20:58:13 +0100

    Updated requirements, and added whitenoise for heroku

--- IntegratedProject/settings.py
+++ IntegratedProject/settings.py
@@ -118,11 +118,22 @@ USE_TZ = True
 
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    STATIC_PATH,
-]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
