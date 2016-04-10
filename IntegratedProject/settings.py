ca3ba13d38ee425b9c216c7e2a41811f06b38ebd
Jack Evans <jack@evans.gb.net>
2016-04-10 21:34:40 +0100

    update heroku config

--- IntegratedProject/settings.py
+++ IntegratedProject/settings.py
@@ -131,7 +131,7 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'spacefinder/static'),
 )
 
 
